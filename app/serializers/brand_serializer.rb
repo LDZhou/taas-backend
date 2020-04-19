3f4c9fe88ce15f1673f6b8a554894b3425c1c4a0
Xinghe Lu <lxinghe@yahoo.com>
2020-04-19 17:15:21 +0800

    Add user_id to brands serializer

--- app/serializers/brand_serializer.rb
+++ app/serializers/brand_serializer.rb
@@ -2,7 +2,7 @@ class BrandSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :brand_type, :brand_type, :address, :contact_name,
     :contact_title, :contact_phone, :contact_email, :city, :province, :business_license,
-    :certificates
+    :certificates, :user_id
 
   attribute :created_at do |b|
     b.created_at_formatted
