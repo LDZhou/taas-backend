13a6931bee21128039460df86936eb7f8f0ad40f
Lu,Xinghe <lxinghe@yahoo.com>
2020-05-20 10:11:05 +0800

    includes app info

--- app/serializers/brand_serializer.rb
+++ app/serializers/brand_serializer.rb
@@ -2,9 +2,13 @@ class BrandSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :brand_type, :brand_type, :address, :contact_name,
     :contact_title, :contact_phone, :contact_email, :city, :province, :business_license,
-    :certificates, :user_id
+    :certificates, :user_id, :app_id
 
   attribute :created_at do |b|
     b.created_at_formatted
+  end
+
+  attribute :app_name do |b|
+    b.application&.name || ''
   end
 end
--- app/serializers/chain_serializer.rb
+++ app/serializers/chain_serializer.rb
@@ -1,6 +1,6 @@
 class ChainSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :cover_photo, :share_photo, :qr_code, :total_views
+  attributes :id, :name, :cover_photo, :share_photo, :qr_code, :total_views, app_id
 
   attribute :created_at do |c|
     c.created_at_formatted
@@ -9,5 +9,9 @@ class ChainSerializer
   attribute :products do |c|
     products = c.chain_products.collect(&:product).compact
     ProductSerializer.new(products).serializable_hash[:data]
+  end
+
+  attribute :app_name do |b|
+    b.application&.name || ''
   end
 end
